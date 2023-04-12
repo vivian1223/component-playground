2faa7c9cbc6ba20e82da2761e712b4fff1b68858
Vivian Yeh <u4h93u6uhu@gmail.com>
2023-04-12 13:35:47 +0800

    fix: add css minify config

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,8 @@ import svgr from 'vite-plugin-svgr';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+	build: {
+    cssMinify: true,
+    cssTarget: 'es2015',
+  },
 });
