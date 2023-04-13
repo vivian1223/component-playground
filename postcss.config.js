6777a76fea215a65ef00db06cf5691f9c16183ea
Vivian Yeh <u4h93u6uhu@gmail.com>
2023-04-13 16:35:34 +0800

    feat: add postcss-preset-env plugin

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+import PostcssPresetEnv from 'postcss-preset-env';
+
+export default {
+	plugins: [
+		PostcssPresetEnv(),
+	],
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
+import postcssPresetEnv from 'postcss-preset-env';
 
 // https://vitejs.dev/config/
 export default defineConfig({
