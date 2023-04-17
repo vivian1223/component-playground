f86bcb92930156eb2526b864d62f63fb02a2e7fb
Vivian Yeh <u4h93u6uhu@gmail.com>
2023-04-17 18:23:07 +0800

    feat: update header compoent

--- src/components/organs/Header/index.tsx
+++ src/components/organs/Header/index.tsx
@@ -3,7 +3,10 @@ import React from 'react';
 import styles from './index.module.css';
 
 export const Header = () => (
-	<>
-		<h1 className={styles.title}>Hi there! Welcome to Vivian&apos;s components demo website!</h1>
-	</>
+	<div className={styles.header}>
+		<h1 className={styles.title}>Practice & Patience</h1>
+		<p>
+			Practice makes perfect, but it's also important to be patient with yourself.<br/> Welcome to Vivian' s component playgroud.
+		</p>
+	</div>
 );
