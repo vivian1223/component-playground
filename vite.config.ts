5b65c33434210d0efb21834821cf3dab7ec41c86
Vivian Yeh <u4h93u6uhu@gmail.com>
2023-04-17 19:37:19 +0800

    ci: add github actions script #2

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import postcssPresetEnv from 'postcss-preset-env';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	base: '/component-playground/',
   plugins: [react(), svgr()],
 	build: {
     cssMinify: true,
